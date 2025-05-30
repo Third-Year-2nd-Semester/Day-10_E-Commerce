31f7472dc63489e8a87b96c977d202553beecfec
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2025-05-30 09:11:37 +0530

    Update GenericExceptionHandler.java

--- myapp/src/main/java/vau/ac/lk/ict/exceptionHandle/GenericExceptionHandler.java
+++ myapp/src/main/java/vau/ac/lk/ict/exceptionHandle/GenericExceptionHandler.java
@@ -1,5 +1,17 @@
 package vau.ac.lk.ict.exceptionHandle;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import vau.ac.lk.ict.model.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
 
 public class GenericExceptionHandler {
-
+@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException exception) {
+		ErrorResponse errorResponce = new ErrorResponse(HttpStatus.NOT_FOUND.value(), 
+				exception.getMessage());
+		return new ResponseEntity<ErrorResponse>(errorResponce, HttpStatus.NOT_FOUND);
+	}
 }
